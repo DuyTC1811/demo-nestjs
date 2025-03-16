f3ad5acae5c1b7a2ec4004f39ab93a6ce7125e15
Duy.T.C <duytran.81811@gmail.com>
2025-03-16 09:49:37 +0700

    [ REFACTOR ]: test logs

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   getHello(): string {
+    this.logger.log('getHello() được gọi'); // Log cấp độ "log"
+    this.logger.warn('Cảnh báo trong getHello()'); // Log cấp độ "warn"
+    this.logger.error('Lỗi giả định trong getHello()'); // Log cấp độ "error"
     return 'Hello World!';
   }
 }
